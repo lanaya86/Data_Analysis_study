39bc83af3fc6f9a4bddf37b57edeab79699bc8b9
lanaya86 <128978703+lanaya86@users.noreply.github.com>
2024-05-21 22:33:28 +0200

    Create povzyk_mockup_005.sql

---
+++ povzyk_mockup_005.sql
@@ -0,0 +1,18 @@
+SELECT *
+FROM food_order fo ;
+
+WITH clients_restaurants_count AS(
+	SELECT customer_id
+		, COUNT(restaurant_name) OVER(PARTITION BY customer_id) AS restaurant_count
+		, restaurant_name 	
+	FROM food_order fo 
+	ORDER BY 2 DESC
+)
+SELECT 
+	customer_id 
+	, COUNT(restaurant_name) restaurant_visit_count
+	, restaurant_name
+	, COUNT (restaurant_name) OVER(PARTITION BY customer_id) AS unique_restaurants_of_customer_count
+FROM clients_restaurants_count 
+GROUP BY customer_id, restaurant_name 
+ORDER BY 4 DESC
