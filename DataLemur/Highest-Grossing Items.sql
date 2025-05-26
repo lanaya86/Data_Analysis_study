c46c20d3937f41ffccf5fddb1a2a56c953eb6690
lanaya86 <128978703+lanaya86@users.noreply.github.com>
2025-05-26 19:40:31 +0200

    Update Highest-Grossing Items.sql

--- DataLemur/Highest-Grossing Items.sql
+++ DataLemur/Highest-Grossing Items.sql
@@ -7,26 +7,19 @@ WITH spend_per_product AS (
     SELECT
         category,
         product,
-        SUM(spend) AS total_spend
+        SUM(spend) AS total_spend,
+        ROW_NUMBER() OVER (
+            PARTITION BY category
+            ORDER BY SUM(spend) DESC
+        ) AS rn
     FROM product_spend
     WHERE DATE_PART('year', transaction_date) = 2022
     GROUP BY category, product
-),
-ranked AS (
-    SELECT
-        category,
-        product,
-        total_spend,
-        ROW_NUMBER() OVER (
-            PARTITION BY category
-            ORDER BY total_spend DESC
-        ) AS rn
-    FROM spend_per_product
 )
 SELECT
     category,
     product,
     total_spend
-FROM ranked
+FROM spend_per_product
 WHERE rn IN (1, 2)
 ORDER BY category, rn;
