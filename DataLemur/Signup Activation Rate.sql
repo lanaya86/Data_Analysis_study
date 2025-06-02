9f086070401438892d630f9cbbb88e088f9decab
lanaya86 <128978703+lanaya86@users.noreply.github.com>
2025-06-02 21:01:41 +0200

    Create Signup Activation Rate.sql

---
+++ DataLemur/Signup Activation Rate.sql
@@ -0,0 +1,11 @@
+--https://datalemur.com/questions/signup-confirmation-rate
+
+--New TikTok users sign up with their emails. They confirmed their signup by replying to the text confirmation to activate their accounts. Users may receive multiple text messages for account confirmation until they have confirmed their new account.
+--A senior analyst is interested to know the activation rate of specified users in the emails table. Write a query to find the activation rate. Round the percentage to 2 decimal places.
+
+SELECT 
+    ROUND(1.00 * COUNT(*) FILTER(WHERE signup_action = 'Confirmed')
+    / COUNT(DISTINCT user_id), 2)
+FROM emails
+LEFT JOIN texts
+USING (email_id)
